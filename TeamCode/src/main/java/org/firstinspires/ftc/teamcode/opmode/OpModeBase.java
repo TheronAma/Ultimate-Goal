c524fe3f81e3a2abaded81db7f91488adc02f1c2
Hydralyncs <39605074+Hydralyncs@users.noreply.github.com>
2020-10-21 17:06:55 -0400

    Implement Shooter velocity control and state update methods.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpModeBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpModeBase.java
@@ -2,7 +2,8 @@ package org.firstinspires.ftc.teamcode.opmode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.subsystem.shooter.Shooter;
+
 //TODO Look into command based opmodes
 public abstract class OpModeBase extends LinearOpMode {
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/shooter/Shooter.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.subsystem.shooter;
 
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.acmerobotics.roadrunner.control.PIDFController;
+import com.acmerobotics.roadrunner.util.NanoClock;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -10,15 +12,41 @@ public class Shooter {
         DcMotorEx flywheel;
         Servo flap;
 
+        double currentTime;
+        double lastTime;
+
+        double offset;
+
+        double velocity;
+        double currentPosition;
+        double lastPosition;
+
+        double targetRpm = 0;
+
+        public static double TICKS_PER_REV = 7;
+
         public static double kV = 0.01;
         PIDCoefficients velocityPID = new PIDCoefficients(1,0,0);
 
         PIDFController velocityController;
 
+        /*
+
+
+
+         */
+
         public Shooter(HardwareMap hwMap){
                 flywheel = hwMap.get(DcMotorEx.class,"flywheel");
                 flap = hwMap.get(Servo.class,"flap");
                 velocityController = new PIDFController(velocityPID,kV);
+
+                offset = flywheel.getCurrentPosition();
+
+                currentTime = System.nanoTime() * Math.pow(10,9);
+
+                lastPosition = 0;
+                currentPosition = 0;
 
         }
 
@@ -27,14 +55,28 @@ public class Shooter {
         }
 
         public void setVelocity(double rpm){
-
+                targetRpm = rpm;
+                velocityController.setTargetPosition(rpm);
         }
 
-        public void setAngle(double angle){
+        public void setAngle(double degrees){
 
         }
-
+        /*
+        The update() method updates the state variables of the flywheel, which include velocity and position
+        It also updates the PID controller for the velocity of the flywheel
+        It also records the time
+        */
         public void update(){
+                currentPosition = flywheel.getCurrentPosition() - offset;
+                currentTime = System.nanoTime() * Math.pow(10,9);
 
+                velocity = (currentPosition-lastPosition)/(currentTime-lastTime);
+
+                double power = velocityController.update(velocity);
+                flywheel.setPower(power);
+
+                lastPosition = currentPosition;
+                lastTime = currentTime;
         }
 }
