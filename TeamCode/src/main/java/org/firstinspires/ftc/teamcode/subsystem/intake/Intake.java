68d59854832113f888b1b3648b51d5dca7646a89
Hydralyncs <39605074+Hydralyncs@users.noreply.github.com>
2020-10-31 11:15:10 -0400

    Add intake setPower methods.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/intake/Intake.java
@@ -1,4 +1,20 @@
 package org.firstinspires.ftc.teamcode.subsystem.intake;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Intake {
+    DcMotorEx frontIntake;
+    DcMotorEx backIntake;
+
+    public Intake(HardwareMap map){
+        frontIntake = map.get(DcMotorEx.class, "frontIntake");
+        backIntake = map.get(DcMotorEx.class, "backIntake");
+
+    }
+
+    public void setPower(double power){
+        frontIntake.setPower(power);
+        backIntake.setPower(power);
+    }
 }
