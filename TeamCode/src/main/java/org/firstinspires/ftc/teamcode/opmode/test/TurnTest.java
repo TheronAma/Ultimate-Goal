1b5d86f3c1894e45f57a157d0fd95b18d8bb6cd4
Hydralyncs <39605074+Hydralyncs@users.noreply.github.com>
2020-10-31 14:11:54 -0400

    Add roadrunner tuning opmodes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/TurnTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.opmode.test;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.subsystem.drivetrain.DriveTrain;
+
+/*
+ * This is a simple routine to test turning capabilities.
+ */
+@Config
+@Autonomous(group = "drive")
+public class TurnTest extends LinearOpMode {
+    public static double ANGLE = 90; // deg
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DriveTrain drive = new DriveTrain(hardwareMap);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        drive.turn(Math.toRadians(ANGLE));
+    }
+}
